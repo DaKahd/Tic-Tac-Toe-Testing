8f133ab491d8d051124a94a5c06ef6f387fe9ccd
DaKahd <90017411+DaKahd@users.noreply.github.com>
2022-11-07 15:27:39 -0800

    Delete GameState.java

--- GameState.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public interface GameState{
-	public boolean isGameover();
-	public Player getWinner();
-	public Player getCurrentPlayer();
-	public ArrayList<String> getCurrentMoves();
-	public void makeMove(String move);
-	public String toString();
-}
